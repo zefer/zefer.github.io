9bc83cb0fecd0fbfcf52dc113373d641dcd36b98
Joe Roberts <joe@zefer.co.uk>
2011-09-24 12:38:42 +0100

    haml views and config

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'http://rubygems.org'
 gem 'sinatra', '~> 1.2.6'
 gem 'thin', '~> 1.2.11'
 
+gem 'haml', '~> 3.1.3'
+
 group :development, :test do
   gem 'rake'
   gem 'foreman', '~> 0.20.0'
--- site.rb
+++ site.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'haml'
+
+configure do
+  set :environment, ( ARGV.index("-e") || ENV["RACK_ENV"] || "development" )
+  ENV['RACK_ENV'] = settings.environment
+  
+  set :root, File.dirname(__FILE__)
+  set :public, File.dirname(__FILE__) + '/assets'
+  set :views, File.dirname(__FILE__) + '/views'
+  
+  set :haml, :format => :html5
+end
 
 get '/' do
-  "Ride on."
+  haml :home
 end
